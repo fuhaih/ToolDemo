7090b06151e7591e765cd1156ba074a426242d8a
fuhaih <1154590036@qq.com>
2017-07-17 14:31:25 +0800

    init project

---
+++ ImageTookit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ImageTookit.Startup))]
+namespace ImageTookit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
